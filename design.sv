dd8e496c94e744d621996b18fa139441acaacaa0
Bishal7-veri <bishal.work25@gmail.com>
2025-06-13 10:56:46 +0530

    Update design.sv

--- design.sv
+++ design.sv
@@ -1,14 +1,21 @@
-module ring_counter (
-    input wire clk,
-    input wire rst,
-    output reg [3:0] Qn
-);
-
-    always @(posedge clk or posedge rst) begin
-        if (rst)
-            Qn <= 4'b1000;  // Initialize with starting pattern
-        else
-            Qn <= {Qn[0], Qn[3:1]};  // Right circular shift
+// ring counter code 
+module ring_counter(
+input wire clk, rst, load,
+  input wire [3:0] D,
+  output reg [3:0] Qn);
+  always @ (posedge clk or posedge rst)
+    begin
+      if(rst)
+        begin
+          Qn <= 4'b0000;
+        end
+      else if (load)
+        begin
+          Qn <= D;
+        end
+      else 
+        begin
+          Qn <= {Qn[0], Qn[3:1]};
+        end
     end
-
 endmodule
