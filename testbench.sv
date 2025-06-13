4f3c5e1f279d34ba57f40a7b03108150dd096e58
Bishal7-veri <bishal.work25@gmail.com>
2025-06-13 10:57:15 +0530

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -1,27 +1,31 @@
 
 
-module tb;
-    reg clk, rst;
-    wire [3:0] Qn;
-
-    ring_counter uut (
-        .clk(clk),
-        .rst(rst),
-        .Qn(Qn)
-    );
-
-    initial begin
-        clk = 0;
-        forever #5 clk = ~clk;
+module tb_ring_counter;
+  reg clk, rst, load;
+  reg [3:0] D;
+  wire [3:0] Qn;
+  
+  ring_counter dut (.clk(clk), .rst(rst), .load(load), .D(D), .Qn(Qn));
+  initial
+    begin
+      clk = 0;
+      forever #5 clk = ~clk;
     end
-
-    initial begin
-        $display("Time\tclk\trst\tQn");
-        $monitor("%0t\t%b\t%b\t%b", $time, clk, rst, Qn);
-
-        rst = 1;
-        #10 rst = 0;
-
-        #100 $finish;
+  initial
+    begin
+      rst = 1; load = 0; D = 4'b0000;
+      #10 rst = 0;
+      #10 D = 4'b1000; load = 1;
+      #10 load = 0;
+      #60 $finish();
+    end
+  initial
+    begin
+      $monitor("Time = %0t | Qn = %b", $time, Qn);
+    end
+  initial
+    begin
+      $dumpfile ("ring_counter.vcd");
+      $dumpvars (0,tb_ring_counter);
     end
 endmodule
